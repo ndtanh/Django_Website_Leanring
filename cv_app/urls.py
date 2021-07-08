19172dc3839f03038ab5c05d2078e49b82e1f913
ndtanh <ndtanh.99@gmail.com>
2021-07-08 13:12:59 +0700

    add view

--- cv_app/urls.py
+++ cv_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cv/<str:phone>/', views.cv, name='cv_app'),
 ]
--- cv_app/views.py
+++ cv_app/views.py
@@ -1,6 +1,28 @@
+from django.db.models import F
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import *
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def cv(request, phone):
+    base_infor = BaseInformation.objects.get(phone=phone)
+    lst_skills = Skill.objects.filter(user=base_infor)
+    lst_education = Education.objects.filter(user=base_infor).order_by(F('end_date').desc())
+    lst_certifications = Certification.objects.filter(user=base_infor).order_by(F('time').desc())
+    lst_projects = Project.objects.filter(user=base_infor).order_by(F('time_end').desc())
+    lst_experiences = Experience.objects.filter(user=base_infor).order_by(F('time_end').desc())
+    data = {
+        'base_infor': base_infor,
+        'lst_skills': lst_skills,
+        'lst_education': lst_education,
+        'lst_certifications': lst_certifications,
+        'lst_projects': lst_projects,
+        'lst_experiences': lst_experiences,
+    }
+    return render(request, 'cv_app/index.html', context=data)
